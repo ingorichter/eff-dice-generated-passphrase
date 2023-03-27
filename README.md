6d976488e4be15edc336d075b449bdab2fe14a83
Ingo Richter <ingo.richter+github@gmail.com>
2023-03-27 10:42:20 +0200

    add copy to clipboard functionality

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 Simple reactjs app implementing the idea of generating a passphrase by rolling some dices. Details are described here https://www.eff.org/dice
 
 # Todo
-- [ ] Add Copy to clipboard
+- [X] Add copy to clipboard
+- [ ] Fade the copy success message after some time
+- [ ] Add reset button
--- src/Passphrase.tsx
+++ src/Passphrase.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './Passphrase.css';
 
 interface PassphraseProps {
@@ -5,22 +6,42 @@ interface PassphraseProps {
 };
 
 const Passphrase = (props: PassphraseProps) => {
+    const [copySuccess, setCopySuccess] = useState('');
+
+    // your function to copy here
+    const copyToClipBoard = async (copyMe: string) => {
+        try {
+            await navigator.clipboard.writeText(copyMe);
+            setCopySuccess('Copied!');
+        } catch (err) {
+            setCopySuccess('Failed to copy!');
+        }
+    };
+
     const passphraseString = props.phrases.filter(phrase => phrase.length > 0).map((phrase, index, array) => {
         const spacer = array.length - 1 === index ? <p></p> : <p>—</p>;
         return (
-            <div key={index} style={{display: 'flex'}}>
+            <div key={index} style={{ display: 'flex' }}>
                 <div className="PassphraseTag">{phrase}</div>
                 {spacer}
             </div>
         );
     });
 
+    const renderCopyButton =
+        (props.phrases.filter(phrase => phrase.length > 0).length > 0) ? 
+            <button onClick={() => copyToClipBoard(props.phrases.join("-"))}>Copy to clipboard</button> : <div></div>;
+
     return (
         <>
             <h1>Passphrase</h1>
-            <div className="Passphrase">
-                {passphraseString}
+            <div className='PassphraseContainer'>
+                <div className="Passphrase">
+                    {passphraseString}
+                </div>
+                {renderCopyButton}
             </div>
+            {copySuccess}
         </>
     );
 };
